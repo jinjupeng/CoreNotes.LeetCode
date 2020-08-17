70fb86a39d5aafd200a7de1f2945e21b8c84fa1c
jinjupeng <im.jp@outlook.com>
2020-08-17 10:33:14 +0800

    新增回文数算法

---
+++ leetcode9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Net.Security;
+
+namespace leetcode9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pro = new Program();
+            var result = pro.IsPalindrome(121);
+            Console.WriteLine("result: ");
+        }
+
+        /**
+         * 回文数
+         */
+        public bool IsPalindrome(int x)
+        {
+            var result = true;
+            var str = x.ToString();
+            if (str.StartsWith('-'))
+            {
+                return false;
+            }
+
+            var reverse = str.Reverse().ToArray();
+            var array = str.ToArray();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (array[i] != reverse[i])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
