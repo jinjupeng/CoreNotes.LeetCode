825f78d1bce115cf024a188adcb46c887fb2fb5a
jinjupeng <im.jp@outlook.com>
2020-08-19 11:52:12 +0800

    28. 实现 strStr()

---
+++ leetcode28/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace leetcode28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 28. 实现 strStr()
+        /// </summary>
+        /// <param name="haystack"></param>
+        /// <param name="needle"></param>
+        /// <returns></returns>
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle))
+            {
+                return 0;
+            }
+
+            if (haystack.Length < needle.Length)
+            {
+                return -1;
+            }
+
+            return haystack.IndexOf(needle);
+        }
+    }
+}
