45b40c902d62218f8acbe2f4ec57001c2537c08c
jinjupeng <im.jp@outlook.com>
2020-08-21 16:31:15 +0800

    374. 猜数字大小

---
+++ leetcode374/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace leetcode374
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 374. 猜数字大小
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int GuessNumber(int n)
+        {
+            // 二分查找
+            var left = 1;
+            var right = n;
+            int mid = 1;
+            while (left <= right)
+            {
+                mid = left + (right - left) / 2;
+                // guess() 方法由leetcode测试系统提供
+                if (guess(mid) == 1)
+                {
+                    left = mid + 1;
+                }
+                else if (guess(mid) == -1)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return mid;
+        }
+    }
+}
