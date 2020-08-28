787fa95fe28951ae96ac51a3ce05b7fa1da70f09
jinjupeng <im.jp@outlook.com>
2020-08-28 18:40:03 +0800

    237.删除链表中的节点

---
+++ leetcode237/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace leetcode237
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 237.删除链表中的节点
+        /// </summary>
+        /// <param name="node"></param>
+        public void DeleteNode(ListNode node)
+        {
+
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
