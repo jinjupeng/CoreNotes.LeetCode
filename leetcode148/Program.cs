d6e3e5e9dd3745e616b18547805ef76c4eca9186
jinjupeng <im.jp@outlook.com>
2020-09-02 16:37:06 +0800

    242. 有效的字母异位词、148. 排序链表

---
+++ leetcode148/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcode148
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pro = new Program();
+            var head = new ListNode(4) {next = new ListNode(2) { next = new ListNode(1) { next = new ListNode(3) } } };
+            var data = pro.SortList(head);
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// [148] 排序链表
+        /// 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+        /// O(n log n)：快速排序、归并排序
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode SortList(ListNode head)
+        {
+            /*
+             * 归并排序版本：
+             * 知识点1：归并排序的整体思想
+             * 知识点2：快慢指针找到链表的中间节点
+             * 知识点3：合并两个已排好序的链表为一个新的有序链表
+             */
+            if (head == null)
+            {
+                return null;
+            }
+
+            return MergeSort(head);
+        }
+
+        private ListNode MergeSort(ListNode head)
+        {
+            if (head.next == null)
+            {
+                return head;
+            }
+            // 找到中间节点
+            var fast = head;
+            var slow = head;
+
+            // 标记节点
+            ListNode sign = null;
+            while (fast.next != null)
+            {
+                if (fast.next.next != null)
+                {
+                    fast = fast.next.next;
+                }
+                else
+                {
+                    fast = fast.next;
+                }
+
+                sign = slow;
+                slow = slow.next;
+            }
+
+            // 断开链表
+            sign.next = null;
+
+            var left = MergeSort(head);
+            var right = MergeSort(slow);
+
+            return Merge(left, right);
+        }
+        private ListNode Merge(ListNode left, ListNode right)
+        {
+            // 临时头节点
+            var resultNode = new ListNode(default);
+            // 链表指针
+            var cur = resultNode;
+            while (left != null && right != null)
+            {
+                if (left.val < right.val)
+                {
+                    cur.next = left;
+                    cur = cur.next;
+                    left = left.next;
+                }
+                else
+                {
+                    cur.next = right;
+                    cur = cur.next;
+                    right = right.next;
+                }
+            }
+
+            if (left != null)
+            {
+                cur.next = left;
+            }
+
+            if (right!= null)
+            {
+                cur.next = right;
+            }
+            return resultNode.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
---
+++ leetcode242/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+
+namespace leetcode242
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pro = new Program();
+            var s = "rat";
+            var t = "car";
+            var data = pro.IsAnagram(s, t);
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 242. 有效的字母异位词
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public bool IsAnagram(string s, string t)
+        {
+            // 思路：字母异位词，也就是两个字符串中的相同字符的数量要对应相等。
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+            Hashtable hashS = new Hashtable(26);
+            Hashtable hashT = new Hashtable(26);
+            var charS = s.ToCharArray();
+            var charT = t.ToCharArray();
+            foreach (var ch in charS)
+            {
+                if (hashS.ContainsKey(ch))
+                {
+                    int i = (int)hashS[ch];
+                    hashS.Remove(ch);
+                    hashS.Add(ch, ++i);
+                }
+                else
+                {
+                    hashS.Add(ch, 1);
+                }
+            }
+            foreach (var ch in charT)
+            {
+                if (hashT.ContainsKey(ch))
+                {
+                    int i = (int)hashT[ch];
+                    hashT.Remove(ch);
+                    hashT.Add(ch, ++i);
+                }
+                else
+                {
+                    hashT.Add(ch, 1);
+                }
+            }
+
+            foreach (char key in hashS.Keys)
+            {
+                if (hashT.ContainsKey(key))
+                {
+                    int a = (int)hashS[key];
+                    int b = (int)hashT[key];
+                    if (a != b)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
