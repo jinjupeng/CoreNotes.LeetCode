51877cf1f61b3bc718ccd3e6abff96e9a8b6405e
jinjupeng <im.jp@outlook.com>
2020-09-16 17:24:52 +0800

    208. 实现 Trie (前缀树)

---
+++ leetcode208/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace leetcode208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrieTest();
+        }
+
+        public static void TrieTest()
+        {
+            Trie trie = new Trie();
+            trie.Insert("apple");
+            var a = trie.Search("apple");   // 返回 true
+            var b = trie.Search("app");     // 返回 false
+            var c = trie.StartsWith("app"); // 返回 true
+            trie.Insert("app");
+            var d = trie.Search("app");     // 返回 true
+
+        }
+    }
+}
