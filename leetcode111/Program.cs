9c6097bdb05a622857b7e66be74472d9047306f4
jinjupeng <im.jp@outlook.com>
2020-09-18 10:37:46 +0800

    111. 二叉树的最小深度

---
+++ leetcode111/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace leetcode111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+            /*
+            var treeNode = new TreeNode(3)
+            {
+                left = new TreeNode(9), right = new TreeNode(20) {left = new TreeNode(15), right = new TreeNode(7)}
+            };
+            */
+            var treeNode = new TreeNode(1) {left = new TreeNode(2)};
+            var data = tree.MinDepth(treeNode);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ leetcode111/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace leetcode111
+{
+    public class Tree
+    {
+
+        /// <summary>
+        /// 111. 二叉树的最小深度
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int MinDepth(TreeNode root)
+        {
+            /*
+             * 最小深度定义为到最近叶子节点的深度,当左右子树都为空时，最小深度才为1，否则最小深度为2
+             */
+            if (root == null)
+            {
+                return 0;
+            }
+            // 思路：深度优先遍历搜索(就是递归)
+            var leftDepth = MinDepth(root.left);
+            var rightDepth = MinDepth(root.right);
+
+            return leftDepth == 0 || rightDepth == 0 ? 1 + leftDepth + rightDepth : 1 + Math.Min(leftDepth, rightDepth);
+        }
+    }
+
+    /// <summary>
+    /// 树节点
+    /// </summary>
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
