0be2bcd8a72fe42cd2f54dcd2614891c944ff5d3
jinjupeng <im.jp@outlook.com>
2020-09-24 08:39:48 +0800

    107. 二叉树的层次遍历 II

---
+++ leetcode107/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace leetcode107
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pro = new Tree();
+
+            var root = new TreeNode(3)
+            {
+                left = new TreeNode(9), right = new TreeNode(20) {left = new TreeNode(15), right = new TreeNode(7)}
+            };
+            /*
+            var root = new TreeNode(1) {left = new TreeNode(2), right = new TreeNode(3)};
+            root.left.left = new TreeNode(4);
+            root.right.right = new TreeNode(5);
+            */
+            pro.LevelOrderBottom(root);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ leetcode107/Tree.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode107
+{
+    public class Tree
+    {
+        /// <summary>
+        /// 107. 二叉树的层次遍历 II
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+            if (root == null)
+            {
+                return list;
+            }
+
+            var queueNodes = new Queue<TreeNode>();
+            queueNodes.Enqueue(root);
+
+            while (queueNodes.Count > 0)
+            {
+                var size = queueNodes.Count;
+                var item = new List<int>();
+                // item.Clear();
+
+                for (var i = 0; i < size; i++)
+                {
+                    var tempNode = queueNodes.Dequeue();
+                    item.Add(tempNode.val);
+                    if (tempNode.left != null)
+                    {
+                        queueNodes.Enqueue(tempNode.left);
+                    }
+
+                    if (tempNode.right != null)
+                    {
+                        queueNodes.Enqueue(tempNode.right);
+                    }
+                }
+                if (item.Count > 0)
+                {
+                    list.Add(new List<int>(item));
+                }
+            }
+
+            var data = list.ToArray().Reverse().ToList();
+            return data;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
